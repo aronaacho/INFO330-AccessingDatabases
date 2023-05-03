dcb5caad1388e37bd24055599ee17e5b4006a483
aronaacho <aronacho@uw.edu>
2023-05-02 20:12:28 -0700

    Updating Changes

--- Java/TeamAnalyzer.java
+++ Java/TeamAnalyzer.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.*;
 
 public class TeamAnalyzer {
     // All the "against" column suffixes:
@@ -17,9 +18,62 @@ public class TeamAnalyzer {
 
         // This bit of JDBC magic I provide as a free gift :-)
         // The rest is up to you.
-        try (Connection con = DriverManager.getConnection("jdbc:sqlite:pokemon.db")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:../pokemon.sqlite")) {
+
             for (String arg : args) {
                 print("Analyzing " + arg);
+                String type1 = "";
+                String type2 = "";
+                try (Statement statement = connection.createStatement()) {
+                    // getting the name of the pokemon with pokedex_number
+                    String nameSql = "SELECT name FROM pokemon WHERE id = " + arg ;
+                    try (ResultSet results = statement.executeQuery(nameSql)) {
+                        while (results.next()) {
+                            String name = results.getString("name");
+                            System.out.print(name);
+                        }
+                    }
+                    // getting the type of the pokemon
+                    System.out.print(" (");
+                    String typeSql = "SELECT name FROM type LEFT OUTER JOIN pokemon_type ON " 
+                                    + "type.id = pokemon_type.type_id WHERE pokemon_id = " + arg;
+                    try (ResultSet results = statement.executeQuery(typeSql)) {
+                        int count = 0;
+                        while (results.next()) {
+                            count++;
+                            if (count % 2 == 1) {
+                                type1 = results.getString("name");
+                                System.out.print(type1 + " ");
+                            } else {
+                                type2 = results.getString("name");
+                                System.out.print(type2);
+                            }
+                        }
+                    }
+
+                    // getting against_XXX of types determining strong against / weak against
+                    System.out.print(") is strong against ");
+                    List<String> strong = new ArrayList<>();
+                    List<String> weak = new ArrayList<>();
+                    String againstSql = "SELECT * FROM pokemon_types_battle_view WHERE type1name = '" 
+                                        + type1 + "' AND type2name = '" + type2 + "'";
+
+                    try (ResultSet results = statement.executeQuery(againstSql)) {
+                        while (results.next()) {
+                            for (int i = 0; i < types.length; i++) {
+                                Double strength = results.getDouble("against_" + types[i]);
+                                if (strength > 1) {
+                                    strong.add("'" + types[i] + "'");
+                                } else if (strength < 1) {
+                                    weak.add("'" + types[i] + "'");
+                                }
+                            }
+                            System.out.print(strong);
+                            System.out.println(" but weak against " + weak);
+                        }
+                    }
+                }
+
 
                 // Analyze the pokemon whose pokedex_number is in "arg"
 
